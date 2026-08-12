9fd25ddef5ea40d8c8cfefdeead09a4d68012f89
Madhurchhetri <tandonnakul987@gmail.com>
2026-08-13 00:00:11 +0530

    login service complete

--- Backend/Authentication/Day2/src/services/auth.service.js
+++ Backend/Authentication/Day2/src/services/auth.service.js
@@ -38,4 +38,40 @@ let registerService = async (data)=>{
     }
 }
 
-module.exports = {registerService}
+let loginService = async (data)=>{
+    try {
+        let {email ,password} = data
+        if(!email || !password) return res.status(400).json({
+        message : "all fields are required "
+    })
+
+    let isExisted = await userModel.findOne({
+        email,
+    })
+
+    if(isExisted) return res.status(401).json({
+        message : "this email already exist"
+    })
+
+    let hasPass = bcrypt.hashSync( password , 10 );
+
+    let newUser = await userModel.create({
+        name, 
+        email,
+        password :hasPass
+    })
+
+    let acessToken = generateAccessToken(newUser._id);
+    let refreshToken = generateRefreshToken(newUser._id);
+
+    return {
+        acessToken , refreshToken , newUser
+    }
+
+    } catch (error) {
+        throw new Error("error", error);
+        
+    }
+}
+
+module.exports = {registerService,loginService }
