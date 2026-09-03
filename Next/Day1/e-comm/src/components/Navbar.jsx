f1dd6d68f2e52a03e1c2fc634256e57fac1eadba
Madhurchhetri <tandonnakul987@gmail.com>
2026-09-03 23:53:42 +0530

    day1 complete next project with authentication complete

--- Next/Day1/e-comm/src/components/Navbar.jsx
+++ Next/Day1/e-comm/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
+"use client";
+
 import Link from 'next/link'
 import React from 'react'
 import { ModeToggle } from './ToggleTheme'
+import { useAuth } from '@/context/authContext';
+import { api } from '@/lib/api';
+
 
 const Navbar = () => {
+
+   const { user, hydrateUser } = useAuth();
+
+  const logoutHandler = async () => {
+    try {
+      await api.post("/api/auth/logout");
+
+      // user ko dobara hydrate karega
+      await hydrateUser();
+
+    } catch (error) {
+      console.log("logout error:", error);
+    }
+  };
+
   return (
     <div className='flex justify-between items-center px-8 py-4 font-semibold'>
         <h1 className='font-bold'>E-coomerce</h1>
@@ -11,7 +31,18 @@ const Navbar = () => {
             <Link href={'/layout/products'}>Products</Link>
         </div>
         <div className='flex justify-center items-center gap-2 cursor-pointer'>
+            {user ? (
+          <button
+            onClick={logoutHandler}
+            className="cursor-pointer"
+          >
+            Logout
+          </button>
+        ) : (
+          <Link href="/login">
             Login
+          </Link>
+        )}
             <ModeToggle/>
         </div>
     </div>
