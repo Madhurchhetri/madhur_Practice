a8fbcf51522c162f3893f1a214df90d61ef600a3
Madhurchhetri <tandonnakul987@gmail.com>
2026-09-10 23:57:47 +0530

    create two more controller in fullstack folder in MCP DAY1

--- fullstack/mcp/day1/backend/src/controller/cat.controller.ts
+++ fullstack/mcp/day1/backend/src/controller/cat.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express"
-import { createCatService } from "../services/cat.service.ts"
+import { createCatService, getAllCatsService, getSingleCatService } from "../services/cat.service.ts"
 
  export const createCatController = async (req:Request , res:Response)=>{
     let result = await createCatService(req.body);
@@ -7,6 +7,26 @@ import { createCatService } from "../services/cat.service.ts"
     return res.status(201).json({
         success: true,
         message: "cat created",
+        data : result
+    })
+}
+
+export const getAllCatsController = async (req:Request , res:Response)=>{
+    let result = await getAllCatsService();
+
+    return res.status(200).json({
+        success: true,
+        message: "cats fetched",
+        data : result
+    })
+}
+
+export const getSingleCatController = async (req:Request , res:Response)=>{
+    let result = await getSingleCatService(req.body);
+
+    return res.status(200).json({
+        success: true,
+        message: "single cat fetched",
         data : result
     })
 }
