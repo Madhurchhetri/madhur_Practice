c634785ce6befb9074e0f5be91bce9dc1d6b2955
Madhurchhetri <tandonnakul987@gmail.com>
2026-09-10 23:53:56 +0530

    services create in fullstack folder in MCP day1

--- fullstack/mcp/day1/backend/src/services/cat.service.ts
+++ fullstack/mcp/day1/backend/src/services/cat.service.ts
@@ -1,5 +1,20 @@
 import catModel from "../models/cat.model.ts"
 
+
+//create service
+
 export const createCatService = async (payload:object)=>{
     return await catModel.create(payload)
+}
+
+//get all cats service
+
+export const getAllCatsService = async ()=>{
+    return await catModel.find();
+}
+
+//getSingleCatService service
+
+export const getSingleCatService = async (id: string)=>{
+    return await catModel.findById(id);
 }
