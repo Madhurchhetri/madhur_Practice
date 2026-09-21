e38d3c1eb92e3af0863b1ac01a522ef7bddf3f21
Madhurchhetri <tandonnakul987@gmail.com>
2026-09-21 17:48:41 +0530

    in fulstack folder capstone2 where we add service and sandbox deployment complete

---
+++ fullstack/capstone2/sandbox/server/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/api/sandbox2/healthz' , (req, res)=>{
+    res.status(200).json({
+        message:"sandbox is perfect healthy",
+        status : 'ok'
+    })
+})
+
+export default app;
+
